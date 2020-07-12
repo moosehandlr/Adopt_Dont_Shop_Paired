508432efbf656300ff0a9e9c5a9cb1b5a08431cb
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-12 11:24:46 -0600

    Add spec for user story 12, Can't Favorite a Pet More Than Once

---
+++ spec/features/favorites/delete_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe "Pet favorites deletion spec" do
+  before :each do
+    @placeholder_image = "generic-image-placeholder.png"
+
+    @shelter1 = Shelter.create(name: "Doggo House",
+                               address: "1323 Paper St",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "000000")
+
+    @doggo = Pet.create(image: @placeholder_image,
+                        name: "Doggo",
+                        approximate_age: 3,
+                        sex: "M",
+                        shelter: @shelter1,
+                        description: "What a cute animal!",
+                        status: "Adoptable")
+
+    @dog_show_page = "/pets/#{@doggo.id}"
+  end
+
+  describe "As a visitor after I've favorited a pet" do
+
+    describe "When I visit that pet's show page " do
+      it "I no longer see a link to favorite that pet" do
+        visit @dog_show_page
+        expect(page).to have_content("Favorites: 0")
+        expect(page).to have_button("Add Pet to Favorites")
+
+        click_button "Add Pet to Favorites"
+        expect(page).to have_content("Favorites: 1")
+
+        expect(page).not_to have_button("Add Pet to Favorites")
+        expect(page).to have_button("Remove Pet from Favorites")
+      end
+    end
+
+    describe "When I visit the favorites index page" do
+      it "I can delete a pet from my favorites" do
+        visit @dog_show_page
+        expect(page).to have_content("Favorites: 0")
+
+        click_button "Add Pet to Favorites"
+        expect(current_path).to eq(@dog_show_page)
+        expect(page).to have_content("Favorites: 1")
+
+        click_button "Remove Pet from Favorites"
+        expect(current_path).to eq(@dog_show_page)
+
+        expect(page).to have_content("#{@doggo.name} has been removed from your favorites")
+        expect(page).to have_content("Favorites: 0")
+
+        expect(page).to have_button("Add Pet to Favorites")
+        expect(page).not_to have_button("Remove Pet from Favorites")
+      end
+    end
+  end
+end
