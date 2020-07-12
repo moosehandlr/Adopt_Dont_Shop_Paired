01fc49dd56daefa3b8a5d607bb6134125ecd8f52
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-12 15:19:25 -0600

    Add models for Application table and PetApplication join table

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
+  has_many :pet_applications
+  has_many :applications, through: :pet_applications
 
   validates_presence_of :image, :name, :sex, :description, :status
   validates :approximate_age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
