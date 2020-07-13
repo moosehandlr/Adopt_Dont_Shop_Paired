e35f0ae3fa2ee89c941bd857defe439687235ee9
Alex Desjardins <ardesjardins91@gmail.com>
2020-07-12 18:24:19 -0600

    Update create application method for sad path.

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,9 +5,22 @@ class ApplicationsController < ApplicationController
   end
 
   def create
+
     selected_pets = params.keys.find_all { |key| key.include?("select-") }
-    selected_pets.each { |pet| favorites.remove_pet(params[pet]) }
-    flash[:submitted] = "Application successfully submitted!"
-    redirect_to "/favorites"
+
+    if applications_params.values.any?(&:empty?) || selected_pets.empty?
+      flash[:submitted] = "Form must be completed and pets selected in order to submit the application."
+      redirect_to "/applications/new"
+    else
+      selected_pets.each { |pet| favorites.remove_pet(params[pet]) }
+      flash[:submitted] = "Application successfully submitted!"
+      redirect_to "/favorites"
+    end
+  end
+
+  private
+
+  def applications_params
+    params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
   end
 end
