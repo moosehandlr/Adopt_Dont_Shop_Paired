0da2f46ac0d3faecdcedba3281bc94257d28ac3a
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-14 15:55:26 -0600

    Add spec for approving pet applications

---
+++ spec/features/pet_application/approve_spec.rb
@@ -0,0 +1,70 @@
+require "rails_helper"
+
+RSpec.describe "Approving an Application Spec" do
+  before :each do
+    @placeholder_image = "generic-image-placeholder.png"
+    # @image_name = @placeholder_image[0...-4]
+
+    @shelter1 = Shelter.create(name: "Doggo House",
+      address: "1323 Paper St",
+      city: "Denver",
+      state: "CO",
+      zip: "000000")
+
+    @doggo = Pet.create(image: @placeholder_image,
+      name: "Doggo",
+      approximate_age: 3,
+      sex: "M",
+      shelter: @shelter1,
+      description: "What a cute animal!",
+      status: "Adoptable")
+
+    @catto = Pet.create(image: @placeholder_image,
+      name: "Catto",
+      approximate_age: 7,
+      sex: "F",
+      shelter: @shelter1,
+      description: "What a cute animal!",
+      status: "Adoptable")
+
+    @pets = [@doggo, @catto]
+
+    @billy_joel = Application.create!(name: "Billy Joel",
+      address: "22 Jump Street",
+      city: "Denver",
+      state: "CO",
+      zip: "80808",
+      phone_number: "000000000",
+      description: "Famous and rich")
+
+    @prince = Application.create!(name: "Prince",
+      address: "22 Jump Street",
+      city: "Denver",
+      state: "CO",
+      zip: "80808",
+      phone_number: "000000000",
+      description: "Rich and famous")
+
+    PetApplication.create!(pet_id: @doggo.id, application_id: @billy_joel.id )
+    # PetApplication.create!(pet_id: @catto.id, application_id: @prince.id )
+  end
+
+  describe "As a visitor" do
+    describe "When I visit an application's show page" do
+      it "I can approve the application for one particular pet" do
+
+        visit "/applications/#{@billy_joel.id}"
+
+        expect(page).to have_link("Approve Application for #{@doggo.name}",
+            href: "/pet_applications/#{@billy_joel.id}/#{@doggo.id}")
+
+        click_link "Approve Application for #{@doggo.name}"
+
+        expect(current_path).to eq("/pets/#{@doggo.id}")
+
+        expect(page).to have_content("Status: Pending")
+        expect(page).to have_content("On hold for Billy Joel")
+      end
+    end
+  end
+end
