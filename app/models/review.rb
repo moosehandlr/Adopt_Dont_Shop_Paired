c777cb52f55c0ee1e5453c4ef5e10f5005507c74
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-15 16:47:19 -0600

    Add feature: shelter show pages have cool stats

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,13 @@ class Review < ApplicationRecord
 
   validates_presence_of :title, :rating, :content
   validates_presence_of :image, allow_nil: true
+
+  def self.average_rating
+    if Review.all.empty?
+      0
+    else
+      average = Review.pluck(:rating).map(&:to_f).sum / Review.all.size
+      average.round(2)
+    end
+  end
 end
