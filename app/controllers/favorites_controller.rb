c85565fb122d2b5fd8401f836ff2ad06f05a09e7
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-15 15:41:00 -0600

    Fix bug: favorites flash message now only displays on the favorites page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,19 +4,19 @@ class FavoritesController < ApplicationController
     pet = Pet.find(params[:pet_id])
     favorites.add_pet(params[:pet_id])
     session[:favorites] = favorites.pets
-    flash[:notice] = "You have added #{pet.name} to your favorites."
+    flash.now[:notice] = "You have added #{pet.name} to your favorites."
     redirect_to "/pets/#{params[:pet_id]}"
   end
 
   def index
     @pending_pets = PetApplication.pets_with_apps
-    flash[:notice] = "You have no favorited pets" if favorites.favorite_pets.empty?
+    flash.now[:notice] = "You have no favorited pets" if favorites.favorite_pets.empty?
   end
 
   def destroy
     pet = Pet.find(params[:pet_id])
     favorites.remove_pet(params[:pet_id])
-    flash[:notice] = "#{pet.name} has been removed from your favorites."
+    flash.now[:notice] = "#{pet.name} has been removed from your favorites."
     redirect_back(fallback_location: "/favorites")
   end
 
