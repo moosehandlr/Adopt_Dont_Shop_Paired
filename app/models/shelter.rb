313a21bbff159e4579e14a0a0635edc4a3e3b544
Alex Desjardins <ardesjardins91@gmail.com>
2020-07-15 14:55:35 -0600

    Update dependency

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,5 @@
 class Shelter < ApplicationRecord
-  has_many :pets, dependent: :delete_all
+  has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :delete_all
 
   validates_presence_of :name, :address, :city, :state, :zip
@@ -7,5 +7,4 @@ class Shelter < ApplicationRecord
   def has_approved_app?
     self.pets.any?(&:has_approved_app?)
   end
-
 end
