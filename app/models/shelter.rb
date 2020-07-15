c5139eae8f011f4966f544b45a542ec69abe4f4a
Alex Desjardins <ardesjardins91@gmail.com>
2020-07-15 12:35:13 -0600

    Update shelter model to delete_all foriegn key relationships when shelter detroyed.

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,6 @@
 class Shelter < ApplicationRecord
-  has_many :pets 
-  has_many :reviews
+  has_many :pets, dependent: :delete_all
+  has_many :reviews, dependent: :delete_all
 
   validates_presence_of :name, :address, :city, :state, :zip
 end
