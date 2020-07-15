28e61a610c9e7bb413710854a3d62bc5ab495452
Alex Desjardins <ardesjardins91@gmail.com>
2020-07-14 22:43:02 -0600

    Create spec shelter creation sad path for incomplete form.

--- spec/features/shelters/create_spec.rb
+++ spec/features/shelters/create_spec.rb
@@ -18,4 +18,22 @@ describe "New Shelter Creation Spec" do
     expect(current_path).to eql "/shelters"
     expect(page).to have_text("Roboto House")
   end
+
+  it "I submit incomplete form and a flash message indicates which field(s) I am missing" do
+    visit "/shelters"
+
+    click_link "New Shelter"
+
+    expect(current_path).to eql "/shelters/new"
+
+    fill_in "Name", with: "Roboto House"
+    fill_in "Address", with: "57799 Lamp Post Road"
+    fill_in "City", with: ""
+    fill_in "State", with: "CO"
+    fill_in "Zip", with: ""
+    click_button("Create Shelter")
+
+    expect(current_path).to eql "/shelters/new"
+    expect(page).to have_text("Please add city, zip information before submitting")
+  end
 end
