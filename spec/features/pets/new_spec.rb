562883be50fae299dc288b9e813bf50e1341c2cd
Alex Desjardins <ardesjardins91@gmail.com>
2020-07-14 22:43:30 -0600

    Create spec pet creation sad path for incomplete form.

--- spec/features/pets/new_spec.rb
+++ spec/features/pets/new_spec.rb
@@ -21,7 +21,7 @@ describe "Shelter Pet Creation Spec" do
 
         click_link "Create Pet"
 
-        expect(current_path).to eql("#{shelter_pets_index}/new")
+        expect(current_path).to eq("#{shelter_pets_index}/new")
 
         fill_in :image, with: image_placeholder
         fill_in "Name", with: "Liono"
@@ -31,7 +31,7 @@ describe "Shelter Pet Creation Spec" do
 
         click_button("Create Pet")
 
-        expect(current_path).to eql shelter_pets_index
+        expect(current_path).to eq("/shelters/#{id}/pets")
 
         liono_image = find("#liono-image")
         expect(liono_image[:src]).to include(image_placeholder)
@@ -40,6 +40,36 @@ describe "Shelter Pet Creation Spec" do
         expect(page).to have_text("Name: Liono")
         expect(page).to have_text("Age: 80")
         expect(page).to have_text("Sex: male")
+      end
+
+      it "I submit incomplete form and a flash message indicates which field(s) I am missing" do
+        liono_house = Shelter.create(
+          name: "Liono House",
+          address: "6126 Library Blvd",
+          city: "Denver",
+          state: "CO",
+          zip: "000000"
+        )
+
+        image_placeholder = "https://www.webfx.com/blog/images/cdn.designinstruct.com/files/582-how-to-image-placeholders/generic-image-placeholder.png"
+
+        visit "/shelters/#{liono_house.id}/pets"
+
+        click_link "Create Pet"
+
+        expect(current_path).to eql("/shelters/#{liono_house.id}/pets/new")
+
+        fill_in :image, with: image_placeholder
+        fill_in "Name", with: ""
+        fill_in "Description", with: "What a ferociously cuddly lion!"
+        fill_in :approximate_age, with: ""
+        fill_in "Sex", with: "male"
+
+        click_button("Create Pet")
+
+        expect(current_path).to eq("/shelters/#{liono_house.id}/pets/new")
+
+        expect(page).to have_text("Please add name, approximate_age information before submitting")
       end
     end
   end
